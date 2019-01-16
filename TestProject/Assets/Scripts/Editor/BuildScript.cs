94d153550e6bbe46f4044064eeac9934c2a46716
Dilmer Valecillos <dilmer@deeproottech.com>
2019-01-16 00:49:04 -0700

    Added Part 2 of Build Pipeline for Asset BundleS

--- TestProject/Assets/Scripts/Editor/BuildScript.cs
+++ TestProject/Assets/Scripts/Editor/BuildScript.cs
@@ -2,10 +2,18 @@
 
 public class BuildScript 
 {
+    [MenuItem("Custom Utilities/Build StandaloneLinux64")]
     static void PerformBuild()
     {
         string[] defaultScene = { "Assets/Scenes/MainScene.unity" };
         BuildPipeline.BuildPlayer(defaultScene, "./builds/game.x86_64",
             BuildTarget.StandaloneLinux64, BuildOptions.None);
+    }
+
+    [MenuItem("Custom Utilities/Build Asset Bundle StandaloneLinux64")]
+    static void PerformAssetBundleBuild()
+    {
+        BuildPipeline.BuildAssetBundles("../AssetBundles/", BuildAssetBundleOptions.ChunkBasedCompression,
+            BuildTarget.StandaloneLinux64);
     }
 }
---
+++ TestProject/Assets/Scripts/LoadAssetBundles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetBundles : MonoBehaviour
+{
+    private AssetBundle assetBundle;
+    public string assetBundlePath;
+    public string cubeName = "Cube_sm";
+    void Start()
+    {
+        assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+        if (assetBundle == null)
+        {
+            Debug.Log("Could not load asset bundle");
+        }
+        else {
+            Debug.Log("Asset bundle was loaded");
+            InstantiateCube();
+        }
+    }
+
+    void InstantiateCube()
+    {
+        var cube = assetBundle.LoadAsset(cubeName);
+        Instantiate(cube);
+    }
+}
