1257684aebe34f6700e815bd6007c811543bc279
Dilmer Valecillos <dilmervst@gmail.com>
2020-02-01 21:55:45 -0700

    Fixed incorrect flag batchmode

--- BuildScripts/deploy.sh
+++ BuildScripts/deploy.sh
@@ -2,5 +2,5 @@ echo Clearning Up Build Directory
 rm -rf ../TestProject/build/
 
 echo Starting Build Process
-'/c/Program Files/Unity/Editor/Unity.exe' -quit -bachmode -projectPath ../TestProject/ -executeMethod BuildScript.PerformBuild
+'/c/Program Files/Unity/Editor/Unity.exe' -quit -batchmode -projectPath ../TestProject/ -executeMethod BuildScript.PerformBuild
 echo Ended Build Process
